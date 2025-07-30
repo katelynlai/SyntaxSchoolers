33fcef31c23dab94021941e94ffd1ccdf64b61a8
Dominic Simpson <db_simpson@yahoo.co.uk>
2025-07-30 22:02:08 +0100

    Logout testing passes

--- frontend/cypress/e2e/logout.cy.js
+++ frontend/cypress/e2e/logout.cy.js
@@ -1,27 +1,26 @@
-const { beforeEach } = require("node:test")
-
-describe('Logout functionality', () => {
+const dashboards = [
+  { name: 'Student', url: 'http://localhost:3000/dashboardPages/user_dashboard.html' }, // student dashboard
+  { name: 'Teacher', url: 'http://localhost:3000/dashboardPages/teacher_dashboard.html' } // teacher dashboard
+];    
+    
+dashboards.forEach(({ name, url }) => {
+  describe(`${name} Dashboard Logout`, () => { // will display either student or teacher in name
     beforeEach(() => {
-        cy.visit('http://localhost:3000/landing_page.html', {
-            onBeforeLoad(win) {
-                win.localStorage.setItem('token', 'fake-jwt-token');
-            }
-        });
+      cy.visit(url, {
+        onBeforeLoad(win) {
+          win.localStorage.setItem('token', 'fake-jwt-token');
+        }
+      });
     });
 
     it('clears token and redirects to login page on logout', () => {
-        cy.window().then((win) => {
-            cy.stub(win.location, 'assign').as('redirect');
-        });
-
-        cy.get('#logout').should('exist').click();
+        cy.get('#logout').click();
+        
+        cy.location('pathname').should('include', '/loginPage/login.html');
 
         cy.window().then((win) => {
             expect(win.localStorage.getItem('token')).to.be.null;
+            });
         });
-
-        cy.get('@redirect').should('have.been.calledWith', '..loginPage/login.html');
-
     });
-
 });
