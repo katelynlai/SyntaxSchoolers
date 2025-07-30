1eaa61e5946403495980dd618e54b7fd996947a9
Katelyn <katelyn.lai@outlook.com>
2025-07-30 20:40:38 +0100

    going back to older code

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./database/connect');
 const userRouter = require("./routers/user")
+const path = require('path'); 
 
+// Serve static files from the frontend directory
 const app = express();
 
 //Middleware
@@ -10,5 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRouter);
+
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 module.exports = app
