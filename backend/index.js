b565745389408372a5ad3399d45359e0603b78d3
OsmanTeklies <155589160+OsmanTeklies@users.noreply.github.com>
2025-07-30 14:14:36 +0100

    osman test commit on this branch

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,6 @@ const app = require('../backend/app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`API listening on port ${PORT}...`);
-    console.log(`Environment: ${process.env.NODE_ENV}`);
+    console.log(`App listening on port ${PORT}...`);
 });
+
