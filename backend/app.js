40e3c385ab97b596a8d140b3de7f541a31cbf5fa
Katelyn <katelyn.lai@outlook.com>
2025-07-30 20:56:14 +0100

    fixing colours

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const db = require('./database/connect');
 const userRouter = require("./routers/user")
 const path = require('path'); 
+const level1Routes = require('./routers/level1Routes');
 
 // Serve static files from the frontend directory
 const app = express();
@@ -11,7 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/users", userRouter);
+app.use("/api/levels", level1Routes);
 
 app.use(express.static(path.join(__dirname, '../frontend')));
 
