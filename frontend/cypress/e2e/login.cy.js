bdc6f5d5e33a271f596b70d049bc2d8a0133944d
Dominic Simpson <db_simpson@yahoo.co.uk>
2025-07-30 22:42:38 +0100

    Modified test to account for whether teacher or student

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -12,16 +12,18 @@ cy.visit('http://localhost:3000/loginPage/login.html');
     const alertStub = cy.stub();
     cy.on('window:alert', alertStub);
 
+    cy.get('form').invoke('attr', 'novalidate', true); // disables browser validation
+
     cy.get('form').within(() => {
     cy.get('button[type="submit"]').click();
   });
 
-    
-  cy.url().should('include', '/loginPage/login.html');
+    cy.wrap(alertStub).should('have.been.called'); // verify alert is triggered   
+    cy.url().should('include', '/loginPage/login.html');
   });
 
-  it('submits form when all fields are valid', () => {
-      cy.intercept('POST', '**/users/login', {
+  it('redirects to user dashboard if role is Student', () => { // if person is a student
+    cy.intercept('POST', '**/users/login', {
       statusCode: 200,
       body: { token: 'fake-jwt-token', role: 'Student' }
     }).as('login');
@@ -39,6 +41,28 @@ cy.visit('http://localhost:3000/loginPage/login.html');
     cy.window().then((win) => {
       expect(win.localStorage.getItem('token')).to.eq('fake-jwt-token');
       expect(win.localStorage.getItem('role')).to.eq('Student');
+    });
+  });
+
+  it('redirects to teacher dashboard if role is Staff', () => { // if person is a teacher
+    cy.intercept('POST', '**/users/login', {
+      statusCode: 200,
+      body: { token: 'fake-jwt-token', role: 'Staff' }
+    }).as('login');
+
+    cy.get('#username-input').type('teachermary');
+    cy.get('#password-input').type('securepass');
+
+    cy.get('form').within(() => {
+      cy.get('button[type="submit"]').click();
+    });
+
+    cy.wait('@login');
+    cy.url().should('include', '/dashboardPages/teacher_dashboard.html');
+
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem('token')).to.eq('fake-jwt-token');
+      expect(win.localStorage.getItem('role')).to.eq('Staff');
     });
   });
 
